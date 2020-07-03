8389b42ac7e0492882f642510d10aba901881f5c
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-07-03 22:23:36 +0200

    :package: New: Basic webpack build for background.js added

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+const path = require('path')
 
+module.exports = {
+  entry: {
+    background: './background.js',
+  },
+  output: {
+    filename: 'background.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
 }
