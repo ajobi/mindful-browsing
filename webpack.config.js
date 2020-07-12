3dd65585c517111b146821fb4b19773e6037c691
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-07-12 11:21:57 +0200

    :package: New: css-loader and style-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
