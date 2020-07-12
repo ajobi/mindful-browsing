01334f767ba2eb4db92be672b479739b8505c2fc
Andrej Bilec <andrej.bilec@smilingwords.eu>
2020-07-12 18:14:32 +0200

    :wrench: Fix: DOM access errors fixed in notifications settings

--- layout/newtab/components/NewTabSettingsNotifications.vue
+++ layout/newtab/components/NewTabSettingsNotifications.vue
@@ -20,29 +20,15 @@
         required: true
       }
     },
-    watch: {
-      backgroundAPI: {
-        immediate: true,
-        handler (backgroundAPI) {
-          if (backgroundAPI === null) {
-            return
-          }
-
-          const SETTINGS_NOTIFICATIONS = document.getElementById('settings_notifications')
-          const SOUNDS_ALLOWED_CHECKBOX = SETTINGS_NOTIFICATIONS.querySelector('input[type="checkbox"')
-
-          SETTINGS_NOTIFICATIONS.querySelector(
-            'span'
-          ).innerText = this.backgroundAPI.SETTINGS.getters.getNotificationInterval()
-          SOUNDS_ALLOWED_CHECKBOX.checked = this.backgroundAPI.SETTINGS.getters.getSoundsAllowed()
-        }
-      }
-    },
     mounted() {
       const SETTINGS_NOTIFICATIONS = document.getElementById('settings_notifications')
-
       const SOUNDS_ALLOWED_CHECKBOX = SETTINGS_NOTIFICATIONS.querySelector('input[type="checkbox"')
       const CUSTOMIZE_NOTIFICATIONS = SETTINGS_NOTIFICATIONS.querySelector('button')
+
+      SETTINGS_NOTIFICATIONS.querySelector(
+        'span'
+      ).innerText = this.backgroundAPI.SETTINGS.getters.getNotificationInterval()
+      SOUNDS_ALLOWED_CHECKBOX.checked = this.backgroundAPI.SETTINGS.getters.getSoundsAllowed()
 
       SOUNDS_ALLOWED_CHECKBOX.addEventListener('click', () => {
         this.backgroundAPI.SETTINGS.mutations.setSoundsAllowed(SOUNDS_ALLOWED_CHECKBOX.checked)
