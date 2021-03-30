d5d2abf6cb561c6314e977fdfaa5a1c4ecf02e9e
Andrej Bilec <andrej.bilec@gmail.com>
2021-03-30 20:51:32 +0200

    :ok_hand: Improve: Duplicate challenge code removed

--- vue/components/warning/Warning.vue
+++ vue/components/warning/Warning.vue
@@ -61,7 +61,6 @@ export default {
     const BREATH_GUIDE = document.getElementById('breath_guide')
     const CHALLENGE = document.getElementById('challenge_task')
     const CHALLENGE_STRING = document.getElementById('challenge_string')
-    const CORRECT_INPUT = document.getElementById('correct_input')
     const CHALLENGE_INPUT = document.getElementById('challenge_input')
 
     let breathingTimeout = null
@@ -104,26 +103,6 @@ export default {
           return initiateBreathing()
         case 'challenge':
           return initiateChallenge()
-      }
-    })
-
-    CHALLENGE_INPUT.addEventListener('input', event => {
-      if (CHALLENGE_STRING.innerText === CHALLENGE_INPUT.value) {
-        proceedButton.style.display = 'initial'
-        cancelButton.style.display = 'initial'
-        CHALLENGE.style.display = 'none'
-      } else {
-        const input = event.target.value
-        let correctInput = ''
-        for (let i = 0; i < input.length; i++) {
-          if (input[i] === CHALLENGE_STRING.innerText[i]) {
-            correctInput += input[i]
-          } else {
-            break
-          }
-        }
-
-        CORRECT_INPUT.innerText = correctInput
       }
     })
 
