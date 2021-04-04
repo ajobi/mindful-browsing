e1a13f1aabecf6b82120a7afa13638ab86bc91e1
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-04 10:25:55 +0200

    :package: New: All dependencies upgraded to the latest versions

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', { loader: 'css-loader', options: { url: false } }]
       }
     ]
   },
