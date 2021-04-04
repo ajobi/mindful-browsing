71b9a6b19627c4747d83c349f2d8bdb6bc341685
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-04 17:36:22 +0200

    :ok_hand: Improve: Storage modified by passing messages to worker

--- vue/store/modules/backgroundAPI.js
+++ vue/store/modules/backgroundAPI.js
@@ -1,3 +1,14 @@
+import {
+  MESSAGE_ID_ADD_BLOCKED_DOMAIN,
+  MESSAGE_ID_CANCEL_REMOVAL,
+  MESSAGE_ID_DELETE_BLOCKED_DOMAIN,
+  MESSAGE_ID_REMOVE_BLOCKED_DOMAIN, MESSAGE_ID_SET_ACTIVE_MECHANISM,
+  MESSAGE_ID_SET_ACTIVE_TASK,
+  MESSAGE_ID_SET_BREATH_COUNT,
+  MESSAGE_ID_SET_CHALLENGE_DIFFICULTY, MESSAGE_ID_SET_NOTIFICATION_INTERVAL,
+  MESSAGE_ID_SET_SOUNDS_ALLOWED
+} from '../../../utils/message'
+
 const state = () => ({
   backgroundAPI: null
 })
@@ -15,17 +26,17 @@ const actions = {
     })
   },
   setActiveTask ({ state }, activeTask) {
-    const { VALIDATORS, SETTINGS } = state.backgroundAPI
+    const { VALIDATORS } = state.backgroundAPI
 
     if (!VALIDATORS.validators.validateActiveTask(activeTask)) {
       alert(VALIDATORS.errorMessages.errorActiveTask())
       return
     }
 
-    SETTINGS.mutations.setActiveTask(activeTask)
+    chrome.runtime.sendMessage({ id: MESSAGE_ID_SET_ACTIVE_TASK, value: activeTask })
   },
   addBlockedDomain ({ state }, domainName) {
-    const { VALIDATORS, SETTINGS, URL } = state.backgroundAPI
+    const { VALIDATORS } = state.backgroundAPI
 
     if (!VALIDATORS.validators.validateDomainName(domainName)) {
       alert(VALIDATORS.errorMessages.errorDomainName(domainName))
@@ -37,52 +48,51 @@ const actions = {
       return
     }
 
-    SETTINGS.mutations.addBlockedDomain(URL.domainNameFromUrl(domainName))
+    chrome.runtime.sendMessage({ id: MESSAGE_ID_ADD_BLOCKED_DOMAIN, value: domainName })
   },
   removeBlockedDomain ({ state }, domainName) {
-    state.backgroundAPI.SETTINGS.mutations.removeBlockedDomain(domainName)
+    chrome.runtime.sendMessage({ id: MESSAGE_ID_REMOVE_BLOCKED_DOMAIN, value: domainName })
   },
   cancelRemoval ({ state }, domainName) {
-    state.backgroundAPI.SETTINGS.mutations.cancelRemoval(domainName)
+    chrome.runtime.sendMessage({ id: MESSAGE_ID_CANCEL_REMOVAL, value: domainName })
   },
   deleteBlockedDomain ({ state }, domainName) {
-    state.backgroundAPI.SETTINGS.mutations.deleteBlockedDomain(domainName)
+    chrome.runtime.sendMessage({ id: MESSAGE_ID_DELETE_BLOCKED_DOMAIN, value: domainName })
   },
   setActiveMechanism ({ state }, activeMechanism) {
-    state.backgroundAPI.SETTINGS.mutations.setActiveMechanism(activeMechanism)
+    chrome.runtime.sendMessage({ id: MESSAGE_ID_SET_ACTIVE_MECHANISM, value: activeMechanism })
   },
   setBreathCount ({ state }, breathCount) {
-    const { VALIDATORS, SETTINGS } = state.backgroundAPI
+    const { VALIDATORS } = state.backgroundAPI
 
     if (!VALIDATORS.validators.validateBreathCount(breathCount)) {
       alert(VALIDATORS.errorMessages.errorBreathCount(breathCount))
       return
     }
 
-    SETTINGS.mutations.setBreathCount(breathCount)
+    chrome.runtime.sendMessage({ id: MESSAGE_ID_SET_BREATH_COUNT, value: breathCount })
   },
   setChallengeDifficulty ({ state }, challengeDifficulty) {
-    const { VALIDATORS, SETTINGS } = state.backgroundAPI
+    const { VALIDATORS } = state.backgroundAPI
 
     if (!VALIDATORS.validators.validateChallengeDifficulty(challengeDifficulty)) {
       alert(VALIDATORS.errorMessages.errorChallengeDifficulty(challengeDifficulty))
       return
     }
 
-    SETTINGS.mutations.setChallengeDifficulty(challengeDifficulty)
+    chrome.runtime.sendMessage({ id: MESSAGE_ID_SET_CHALLENGE_DIFFICULTY, value: challengeDifficulty })
   },
   setSoundsAllowed ({ state }, soundsAllowed) {
-    state.backgroundAPI.SETTINGS.mutations.setSoundsAllowed(soundsAllowed)
+    chrome.runtime.sendMessage({ id: MESSAGE_ID_SET_SOUNDS_ALLOWED, value: soundsAllowed })
   },
   setNotificationInterval ({ state }, notificationInterval) {
-    const { VALIDATORS, SETTINGS } = state.backgroundAPI
+    const { VALIDATORS } = state.backgroundAPI
 
     if (!VALIDATORS.validators.validateNotificationInterval(notificationInterval)) {
       alert(VALIDATORS.errorMessages.errorNotificationInterval(notificationInterval))
       return
     }
-
-    SETTINGS.mutations.setNotificationInterval(notificationInterval)
+    chrome.runtime.sendMessage({ id: MESSAGE_ID_SET_NOTIFICATION_INTERVAL, value: notificationInterval })
   }
 }
 
--- worker/storage.js
+++ worker/storage.js
@@ -1,4 +1,19 @@
-import { MESSAGE_ID_STORAGE_UPDATE_REQUEST, MESSAGE_ID_STORAGE_UPDATED } from '../utils/message'
+import { SETTINGS } from './settings'
+import { URL } from './utils/url.js'
+import {
+  MESSAGE_ID_ADD_BLOCKED_DOMAIN,
+  MESSAGE_ID_CANCEL_REMOVAL,
+  MESSAGE_ID_DELETE_BLOCKED_DOMAIN,
+  MESSAGE_ID_REMOVE_BLOCKED_DOMAIN,
+  MESSAGE_ID_SET_ACTIVE_MECHANISM,
+  MESSAGE_ID_SET_ACTIVE_TASK,
+  MESSAGE_ID_SET_BREATH_COUNT,
+  MESSAGE_ID_SET_CHALLENGE_DIFFICULTY,
+  MESSAGE_ID_SET_NOTIFICATION_INTERVAL,
+  MESSAGE_ID_SET_SOUNDS_ALLOWED,
+  MESSAGE_ID_STORAGE_UPDATE_REQUEST,
+  MESSAGE_ID_STORAGE_UPDATED
+} from '../utils/message'
 
 chrome.storage.onChanged.addListener(() => {
   chrome.storage.sync.get(null, storage => {
@@ -11,5 +26,56 @@ chrome.runtime.onMessage.addListener(({ id }) => {
     chrome.storage.sync.get(null, storage => {
       chrome.runtime.sendMessage({ id: MESSAGE_ID_STORAGE_UPDATED, storage })
     })
+  }
+})
+
+chrome.runtime.onMessage.addListener(({ id, value }) => {
+  if (id === MESSAGE_ID_SET_ACTIVE_TASK) {
+    SETTINGS.mutations.setActiveTask(value)
+    return
+  }
+
+  if (id === MESSAGE_ID_ADD_BLOCKED_DOMAIN) {
+    SETTINGS.mutations.addBlockedDomain(URL.domainNameFromUrl(value))
+    return
+  }
+
+  if (id === MESSAGE_ID_REMOVE_BLOCKED_DOMAIN) {
+    SETTINGS.mutations.removeBlockedDomain(value)
+    return
+  }
+
+  if (id === MESSAGE_ID_CANCEL_REMOVAL) {
+    SETTINGS.mutations.cancelRemoval(value)
+    return
+  }
+
+  if (id === MESSAGE_ID_DELETE_BLOCKED_DOMAIN) {
+    SETTINGS.mutations.deleteBlockedDomain(value)
+    return
+  }
+
+  if (id === MESSAGE_ID_SET_ACTIVE_MECHANISM) {
+    SETTINGS.mutations.setActiveMechanism(value)
+    return
+  }
+
+  if (id === MESSAGE_ID_SET_BREATH_COUNT) {
+    SETTINGS.mutations.setBreathCount(value)
+    return
+  }
+
+  if (id === MESSAGE_ID_SET_CHALLENGE_DIFFICULTY) {
+    SETTINGS.mutations.setChallengeDifficulty(value)
+    return
+  }
+
+  if (id === MESSAGE_ID_SET_SOUNDS_ALLOWED) {
+    SETTINGS.mutations.setSoundsAllowed(value)
+    return
+  }
+
+  if (id === MESSAGE_ID_SET_NOTIFICATION_INTERVAL) {
+    SETTINGS.mutations.setNotificationInterval(value)
   }
 })
