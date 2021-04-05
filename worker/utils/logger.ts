56794dc8558301ccf6ba0f6c7ec69267ee8fe581
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-05 15:36:10 +0200

    :package: New: Logger types added

--- worker/utils/logger.ts
+++ worker/utils/logger.ts
@@ -1,13 +1,15 @@
-// @ts-nocheck
 class NamedLogger {
-  constructor (name, color) {
+  readonly name: string
+  readonly color: string
+
+  constructor (name: string, color: string) {
     this.name = name
     this.color = color
   }
 
-  log (message) {
+  log (message: string) {
     console.log(`%c ${this.name}: ${message}`, `color: ${this.color}`)
   }
 }
 
-export const getNamedLogger = (name, color) => new NamedLogger(name, color)
+export const getNamedLogger = (name: string, color: string): NamedLogger => new NamedLogger(name, color)
