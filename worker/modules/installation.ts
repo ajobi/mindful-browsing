907ff0c4417190dbabb68f93f88163503eb2c4ee
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-05 16:11:21 +0200

    :package: New: Installation module types added

--- worker/modules/installation.ts
+++ worker/modules/installation.ts
@@ -1,17 +1,8 @@
-// @ts-nocheck
 // eslint-disable-next-line no-unused-vars
 import { settingsDevelopment, settingsProduction } from '../../interface/settings'
 
-export const install = callback => {
-  chrome.storage.sync.set(settingsDevelopment, () => {
-    // installationLog.log(`Installed with ${initialSettings.targetEnv} settings.`)
-
-    if (callback) {
-      callback()
-    }
-  })
-}
-
 chrome.runtime.onInstalled.addListener(() => {
-  install(() => chrome.tabs.create({ url: 'chrome://newtab/' }))
+  chrome.storage.sync.set(settingsDevelopment, () => {
+    chrome.tabs.create({ url: 'chrome://newtab/' })
+  })
 })
