be76a875e96bd13e72831a7391989eefdbc5673b
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-29 20:06:35 +0200

    :package: New: Tailwind colors and font sizes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
-    extend: {}
+    colors: {
+      primary: {
+        100: '#DADAE6',
+        200: '#A2A3BF',
+        300: '#7F7FA7',
+        400: '#6C6D9A',
+        500: '#5A5B8E',
+        600: '#474881'
+      },
+      white: '#FFFFFF'
+    },
+    fontSize: {
+      xxs: '12px',
+      xs: '14px',
+      sm: '16px',
+      base: '18px',
+      l: '20px',
+      xl: '24px',
+      '2xl': '32px',
+      '3xl': '36px'
+    }
   },
   variants: {
     extend: {}
