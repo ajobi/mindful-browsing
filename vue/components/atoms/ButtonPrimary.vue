8fc6d9a1ae1ab1c4858065e415a5573715ca59a5
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-02 19:12:43 +0200

    :ok_hand: Improve: Primary button styles added in tailwind

--- vue/components/atoms/ButtonPrimary.vue
+++ vue/components/atoms/ButtonPrimary.vue
@@ -1,5 +1,8 @@
 <template>
-  <button class="button--primary focus:outline-none uppercase tracking-tight text-base">
+  <button
+    class="focus:outline-none uppercase tracking-tight text-base border-none text-white bg-primary-400 hover:bg-primary-500 transition-all duration-200 rounded-full"
+    :style="{ height: '35px' }"
+  >
     <slot />
   </button>
 </template>
